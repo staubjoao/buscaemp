8208585c1d0a09513dbf3bafc776fd76b3ea2c07
unknown <staub.joao.cs@gmail.com>
2020-08-11 17:19:31 -0300

    Input's da empresa

--- web/src/pages/register/Company/index.tsx
+++ web/src/pages/register/Company/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import PageHeader from "../../../components/PageHeader";
 
 import "./styles.css";
+import Input from "../../../components/Input";
 
 function Company() {
   return (
@@ -13,8 +14,35 @@ function Company() {
       />
       <main>
         <fieldset>
-          <legend>Dados da sua empresa</legend>
+          <legend>Dados de login</legend>
         </fieldset>
+        <Input label="Email" name="email" />
+        <Input label="Senha" name="password" type="password" />
+        <Input
+          label="Confirmar senha"
+          name="confirmed-password"
+          type="password"
+        />
+
+        <fieldset>
+          <legend>Endereço da sua empresa</legend>
+        </fieldset>
+        <Input label="CEP" name="cep" />
+        <Input label="Estado" name="state" />
+        <Input label="Cidade" name="city" />
+        <Input label="Rua" name="street" />
+        <Input
+          label="Numero"
+          name="address-number"
+        />
+
+        <fieldset>  
+          <legend>Informaçoes da sua empresa</legend>
+        </fieldset>
+        <Input label="Nome da Empresa" name="name" />
+        <Input label="Número" name="number" />
+        <Input label="Ramo de atividade" name="industry" />
+        <Input label="CNPJ" name="cnpj" />
       </main>
     </div>
   );
