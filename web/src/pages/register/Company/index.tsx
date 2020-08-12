0484d57297c61373db464054727fbb7c78eb1f36
unknown <staub.joao.cs@gmail.com>
2020-08-12 17:18:06 -0300

    Estilização do botão

--- web/src/pages/register/Company/index.tsx
+++ web/src/pages/register/Company/index.tsx
@@ -15,37 +15,41 @@ function Company() {
       <main>
         <fieldset>
           <legend>Dados de login</legend>
+          <Input label="Email" name="email" shouldHide={false} />
+          <Input
+            label="Senha"
+            name="password"
+            type="password"
+            shouldHide={false}
+          />
+          <Input
+            label="Confirmar senha"
+            name="confirmed-password"
+            type="password"
+            shouldHide={false}
+          />
         </fieldset>
-        <Input label="Email" name="email" shouldHide={false} />
-        <Input
-          label="Senha"
-          name="password"
-          type="password"
-          shouldHide={false}
-        />
-        <Input
-          label="Confirmar senha"
-          name="confirmed-password"
-          type="password"
-          shouldHide={false}
-        />
 
         <fieldset>
           <legend>Endereço da sua empresa</legend>
+          <Input label="CEP" name="cep" shouldHide={false} />
+          <Input label="Estado" name="state" shouldHide={false} />
+          <Input label="Cidade" name="city" shouldHide={false} />
+          <Input label="Rua" name="street" shouldHide={false} />
+          <Input label="Número" name="address-number" shouldHide={false} />
         </fieldset>
-        <Input label="CEP" name="cep" shouldHide={false} />
-        <Input label="Estado" name="state" shouldHide={false} />
-        <Input label="Cidade" name="city" shouldHide={false} />
-        <Input label="Rua" name="street" shouldHide={false} />
-        <Input label="Numero" name="address-number" shouldHide={false} />
 
         <fieldset>
           <legend>Informaçoes da sua empresa</legend>
+          <Input label="Nome da Empresa" name="name" shouldHide={false} />
+          <Input label="Número" name="number" shouldHide={false} />
+          <Input label="Ramo de atividade" name="industry" shouldHide={false} />
+          <Input label="CNPJ" name="cnpj" shouldHide={false} />
         </fieldset>
-        <Input label="Nome da Empresa" name="name" shouldHide={false} />
-        <Input label="Número" name="number" shouldHide={false} />
-        <Input label="Ramo de atividade" name="industry" shouldHide={false} />
-        <Input label="CNPJ" name="cnpj" shouldHide={false} />
+        
+        <footer>
+          <button type="button">Salvar cadastro</button>
+        </footer>
       </main>
     </div>
   );
